bb82173fae49c823ae47c83060ce6233aa26d3f4
kunal <86314140+kunals131@users.noreply.github.com>
2023-12-30 13:52:44 +0530

    fix: docker file

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ WORKDIR /app
 
 COPY requirements.txt .
 RUN pip install -r requirements.txt
-RUN pip install uvicorn pinecone-client pandas
+RUN pip install uvicorn pinecone-client pandas pymysql
 
 COPY . .
 
--- app/main.py
+++ app/main.py
@@ -7,8 +7,8 @@ from app.config.AppConfig import config
 app = FastAPI(title=config.app_name)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=config.origins,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
